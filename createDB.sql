101c3c043fb0934083c101653136ab146f8b3416
SFergusonEstio <steph.ferguson@estio.co.uk>
2024-10-31 16:12:11 +0000

    Create createDB.sql

---
+++ createDB.sql
@@ -0,0 +1,12 @@
+
+-- Create a new database called RelationalTest
+
+-- Create the following tables (choosing appropriate datatypes for the fields):
+-- Customer with ID, FirstName and Surname as fields
+-- Order with ID, OrderDate, ProductID, CustomerID, Quantity and OrderValue as fields
+-- Product with ID, ProductName and Price as fields 
+
+-- Identify how you are linking the above 3 tables and set primary and foreign keys as appropriate
+
+
+-- EXTENSION: Create the databases that you normalised earlier
