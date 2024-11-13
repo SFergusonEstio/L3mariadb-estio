62adf6668aa50a2b72982af2ea46561a436d5333
SFergusonEstio <steph.ferguson@estio.co.uk>
2024-11-13 10:37:04 +0000

    Update createDB.sql

--- createDB.sql
+++ createDB.sql
@@ -2,9 +2,9 @@
 -- Create a new database called RelationalTest
 
 -- Create the following tables (choosing appropriate datatypes for the fields):
--- Customer with ID, FirstName and Surname as fields
--- Order with ID, OrderDate, ProductID, CustomerID, Quantity and OrderValue as fields
--- Product with ID, ProductName and Price as fields 
+-- Customers with ID, FirstName and Surname as fields
+-- Orders with ID, OrderDate, ProductID, CustomerID, Quantity and OrderValue as fields
+-- Products with ID, ProductName and Price as fields 
 
 -- Identify how you are linking the above 3 tables and set primary and foreign keys as appropriate
 
